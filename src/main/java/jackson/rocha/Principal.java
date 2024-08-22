c85ece9dd6c801e7973906ab8d40647ac63c2b52
jacksonrocha <jackson_lbr@hotmail.com>
2024-08-21 22:07:46 -0300

    Criando o projeto

---
+++ src/main/java/jackson/rocha/Principal.java
@@ -0,0 +1,21 @@
+package jackson.rocha;
+
+public class Principal {
+    public static void main(String[] args) {
+        CarrinhoDeCompra carrinho = new CarrinhoDeCompra();
+//        carrinho.adicionarItem("Água", 5, 4);
+
+        Produto produto = new Produto();
+        produto.nome = "Água";
+        produto.precoUnitario = 5;
+
+
+        carrinho.adicionarItem(produto, 4);
+
+//        produto.alterarStatus(false);
+
+        produto.ativar();
+        produto.desativar();
+    }
+
+}
