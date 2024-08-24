0d6886f4c4faf59ab547f9b630c969204ae906ff
jacksonrocha <jackson_lbr@hotmail.com>
2024-08-23 21:36:33 -0300

    Alterando Produto

--- src/main/java/jackson/rocha/Produto.java
+++ src/main/java/jackson/rocha/Produto.java
@@ -4,24 +4,5 @@ public class Produto {
 
     String nome;
     double precoUnitario;
-    boolean ativo;
 
-    void alterarStatus(boolean ativo) {
-        if (ativo) {
-            System.out.println("Ativando");
-
-        } else {
-            System.out.println("Desativando");
-        }
-
-        this.ativo = ativo;
-    }
-
-    void ativar() {
-        System.out.println("Ativando");
-    }
-
-    void desativar() {
-        System.out.println("Desativando");
-    }
 }
