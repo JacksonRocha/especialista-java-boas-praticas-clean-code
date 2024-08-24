395e06d7236a0e64917f8a06c8d80db14b932919
jacksonrocha <jackson_lbr@hotmail.com>
2024-08-23 21:36:10 -0300

    Alterando CarrinhoDeCompra

--- src/main/java/jackson/rocha/CarrinhoDeCompra.java
+++ src/main/java/jackson/rocha/CarrinhoDeCompra.java
@@ -1,11 +1,33 @@
 package jackson.rocha;
 
+import java.util.Objects;
+
 public class CarrinhoDeCompra {
 
-    void adicionarItem(String nomeProduto, double precoUnitario, int quantidade) {
-        // TODO implementar
-    }
+    Produto produto;
+    int quantidadeItens;
 
     void adicionarItem(Produto produto, int quantidade) {
+        Objects.requireNonNull(produto, "Produto deve ser informado");
+
+        if (quantidade <= 0) {
+            throw new IllegalArgumentException("Quantidade deve ser maior que zero");
+        }
+
+        System.out.printf("Produto %s%n", produto.nome);
+        System.out.printf("Produto %s%n", produto.precoUnitario);
+    }
+
+    void gerarPedido(Endereco enderecoEntrega) {
+        Objects.requireNonNull(enderecoEntrega, "Endereço de entrega deve ser informado");
+
+        criarNovoPedido(enderecoEntrega);
+    }
+
+    void gerarPedido() {
+        criarNovoPedido(null);
+    }
+
+    private void criarNovoPedido(Endereco enderecoEntrega) {
     }
 }
